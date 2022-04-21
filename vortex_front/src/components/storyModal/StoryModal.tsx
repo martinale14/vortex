42bec134a786ff43f5927a8b8be4680367d19bc7
Juan Pablo Rivera <juanpari1766@gmail.com>
2022-04-21 15:10:21 -0500

    StoryModal

--- vortex_front/src/components/storyModal/StoryModal.tsx
+++ vortex_front/src/components/storyModal/StoryModal.tsx
@@ -1,8 +1,6 @@
 import styles from './StoryModal.module.css';
 import userStory from '../../assets/userStory.svg';
 import line from '../../assets/lineOrange.svg';
-/* import trash from '../../assets/trash.svg';
-import add from '../../assets/add.svg'; */
 import exit from '../../assets/exit.svg';
 import Input from '../input/Input';
 import Button from '../button/Button';
@@ -20,13 +18,32 @@ function StoryModal(props: storyProps) {
                 </div>
                 <img src={line} className={styles.vortex_imgLine} alt="line"/>
                 <div className={styles.vortex_cardStory_body}>
-                    <Input type='text' placeholder='Título' label='Título *' onChange={(e : any) => {}}/>
-                    <Input type='text' placeholder='Encargado' label='Empresa' onChange={(e : any) => {}}/>
-                    <Input type='text' placeholder='Sprint ##' label='Sprint' onChange={(e : any) => {}}/>
-                    <Input type='text' placeholder='Ingrese una descripción' label='Descripción*' onChange={(e : any) => {}}/>
+                    <Input className={styles.vortex_input_left} type='text' placeholder='Título' label='Título *' onChange={(e : any) => {}}/>
+                    <Input className={styles.vortex_input_left} type='text' placeholder='Encargado' label='Empresa' onChange={(e : any) => {}}/>
+                    <Input className={styles.vortex_input_left} type='text' placeholder='Sprint ##' label='Sprint' onChange={(e : any) => {}}/>
+                    <Input 
+                        className={styles.vortex_input_right} 
+                        type='text' 
+                        placeholder='Ingrese una descripción' 
+                        label='Descripción*' 
+                        onChange={(e : any) => {}}
+                        inputStyle={styles.vortex_input_desc}
+                        textArea
+                    />
+                    <div className={styles.vortex_acc} >
+                        <Input 
+                            className={styles.vortex_input_right} 
+                            type='text' 
+                            placeholder='Descripción' 
+                            label='Añadir criterio de aceptación*' 
+                            onChange={(e : any) => {}}
+                            inputStyle={styles.vortex_input_desc}
+                            textArea
+                        />
+                    </div>
                 </div>
                 <div className={styles.vortex_button_save}>
-                    <Button /* text='Guardar' */></Button>
+                    <Button text='Guardar'></Button>
                 </div>
             </div>
         </div>
