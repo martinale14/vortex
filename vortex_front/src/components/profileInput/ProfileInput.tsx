a1825ccea6c4b0a8c6f3060ee05bcecd75c80c3b
juanp.rivera <juanp.rivera@veevart.com>
2022-05-22 08:26:02 -0500

    Fix profile view inputs

--- vortex_front/src/components/profileInput/ProfileInput.tsx
+++ vortex_front/src/components/profileInput/ProfileInput.tsx
@@ -1,15 +1,20 @@
+import { ChangeEventHandler } from 'react';
 import styles from './ProfileInput.module.css'
 
 interface propsProfileInput{
+    type: string;
     label: string;
-    text: string;
+    text?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+    defaultValue?: string;
+    readOnly?: boolean;
 }
 
 function ProfileInput(props: propsProfileInput){
     return(
         <div className={styles.vortex_profile_input}>
             <p>{props.label}</p>
-            <input type="text" value={props.text}/>
+            <input type={props.type} readOnly={props.readOnly} defaultValue={props.defaultValue} value={props.text} onChange={props.onChange}/>
         </div>
     )
 }
